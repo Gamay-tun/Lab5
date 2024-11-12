d9c64e802285a3e8ea7707a4e476c92a809b2de7
Gamay-tun <GAMALIELHTTUN07.23@ichat.sp.edu.sg>
2024-11-12 17:24:50 +0800

    Update adc_servo_controller.py

--- adc_servo_controller.py
+++ adc_servo_controller.py
@@ -1,3 +1,6 @@
+#need to revise and test this file
+#the servo is not working properly controlled by the pentiometer 
+
 from threading import Thread
 from hal import hal_adc as adc
 from hal import hal_servo as servo
